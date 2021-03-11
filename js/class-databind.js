096530ef585e872db904e202439695c7c2787c9f
himizu37458 <zarasarewimper@gmai.com>
2021-03-11 18:19:53 +0900

    クラスのデータバインディング

---
+++ js/class-databind.js
@@ -0,0 +1,29 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    // isLarge: true,
+    // haserror: true,
+    // largeClass: 'large',
+    // dangerClass: 'text-danger',
+    // classObject: {
+    //   large: true,
+    //   // クラス名に-があるため''で囲んでいる
+    //   'text-blue': true
+    // },
+    //   largeClass: {
+    //     large: true,
+    //     'bg-gray': true
+    //   },
+    //   dangerClass: {
+    //     'text-danger': true
+    //   },
+    //   isLarge: true
+    // },
+    // color: 'blue',
+    // fontSize: 36,
+    styleObject: {
+      color: 'red',
+      'font-size': '36px'
+    }
+  }
+})
