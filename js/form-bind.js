89a4cbce402966826f33170479571200f2afa294
himizu37458 <zarasarewimper@gmai.com>
2021-03-12 20:20:13 +0900

    フォーム入力バインディング

---
+++ js/form-bind.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    message: '',
+    checked: false,
+    colors: [],
+    radioColor: '',
+    selected: '',
+    colorSelect: [],
+    age: 0
+  }
+})
